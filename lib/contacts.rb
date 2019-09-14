70c9848095d693f020d693c783665043e2a5e827
KG ZZ <czheng5h88@gmail.com>
2019-09-14 16:00:50 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,10 +16,12 @@ require 'pry'
   
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
-  if person == "Freddy Mercury"
-    contact_details_hash.each do |attribute, data|
-      binding.pry
+    if person == "Freddy Mercury"
+      contact_details_hash.each do |attribute, data|
+        if attribute == :favorite_ice_cream_flavors
+          binding.pry
+        end
+      end
     end
-  end
-end
+  lend
 end
